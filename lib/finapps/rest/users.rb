74313019624563cc14454865e2853c496a26bbd9
qbantek <qbantek@gmail.com>
2014-07-11 12:19:08 -0400

    wip

--- lib/finapps/rest/users.rb
+++ lib/finapps/rest/users.rb
@@ -18,17 +18,9 @@ module FinApps
       # @return [FinApps::REST::User, Array<String>]
       def login(params = {})
         logger.debug "##{__method__.to_s} => Started"
-<<<<<<< HEAD
-=======
-
->>>>>>> c8eb56c73da0329b3834b9f7f3c985dfbb503a0b
         user, error_messages = @client.post(END_POINTS[:users_login], params) { |r| User.new(r.body) }
         logger.debug "##{__method__.to_s} => Completed"
 
-<<<<<<< HEAD
-=======
-        logger.debug "##{__method__.to_s} => Completed"
->>>>>>> c8eb56c73da0329b3834b9f7f3c985dfbb503a0b
         return user, error_messages
       end
 
@@ -36,21 +28,10 @@ module FinApps
       # @return [Array<String>]
       def delete(public_id)
         logger.debug "##{__method__.to_s} => Started"
-<<<<<<< HEAD
-=======
-
->>>>>>> c8eb56c73da0329b3834b9f7f3c985dfbb503a0b
         raise MissingArgumentsError.new 'Missing argument: public_id.' if public_id.blank?
         _, error_messages = @client.delete(END_POINTS[:users_delete].sub! ':public_id', public_id.to_s)
         logger.debug "##{__method__.to_s} => Completed"
 
-<<<<<<< HEAD
-=======
-        path = END_POINTS[:users_delete].sub! ':public_id', public_id.to_s
-        _, error_messages = @client.delete(path, {})
-
-        logger.debug "##{__method__.to_s} => Completed"
->>>>>>> c8eb56c73da0329b3834b9f7f3c985dfbb503a0b
         error_messages
       end
 
