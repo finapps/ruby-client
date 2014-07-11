57f45704f59be0ec40d61d83d6bfd82907dc8981
qbantek <qbantek@gmail.com>
2014-07-11 12:22:28 -0400

    wip

--- lib/finapps/rest/resources.rb
+++ lib/finapps/rest/resources.rb
@@ -1,6 +1,7 @@
 module FinApps
   module REST
     class Resources
+      
       include FinApps::REST::Defaults
       include FinApps::Logging
 
@@ -8,21 +9,7 @@ module FinApps
       # @return [FinApps::REST::Resources]
       def initialize(client)
         @client = client
-<<<<<<< HEAD
         logger.debug "##{__method__.to_s} => #{self.class.name} initialized."
-=======
-        # @logger = client.logger || begin
-        #   require 'logger' unless defined?(::Logger)
-        #   ::Logger.new(STDOUT).tap do |log|
-        #     # noinspection SpellCheckingInspection
-        #     log.progname = self.class.name
-        #     log.debug '#initialize => Logger instance created'
-        #   end
-        #
-        #   @logger.debug "#{self.class.name}#initialize => Initialized."
-        # end
-
->>>>>>> c8eb56c73da0329b3834b9f7f3c985dfbb503a0b
       end
 
     end
