7f9a7573db7fcf6a33eec0a674dcb7276e1f0a20
qbantek <qbantek@gmail.com>
2014-07-11 12:21:23 -0400

    wip

--- lib/finapps/rest/connection.rb
+++ lib/finapps/rest/connection.rb
@@ -12,17 +12,12 @@ module FinApps
         logger.debug "##{__method__.to_s} => Started"
 
         company_credentials.validate_required_strings!
-<<<<<<< HEAD
         logger.debug "##{__method__.to_s} => company_credentials passed validation."
-=======
-        logger.debug ' * company credentials format validated'
->>>>>>> c8eb56c73da0329b3834b9f7f3c985dfbb503a0b
 
         host = config[:host]
         validate_host_url! host
 
         base_url = "#{host}/v#{API_VERSION}"
-<<<<<<< HEAD
         logger.debug " base_url: #{base_url}"
 
         timeout = config[:timeout].blank? ? DEFAULTS[:timeout] : config[:timeout]
@@ -33,18 +28,6 @@ module FinApps
 
         user_token = config[:user_token]
         logger.debug " user_token: #{user_token}" if user_token.present?
-=======
-        logger.debug " * base_url: #{base_url}"
-
-        timeout = config[:timeout].blank? ? DEFAULTS[:timeout] : config[:timeout]
-        logger.debug " * timeout: #{timeout}"
-
-        user_identifier = config[:user_identifier]
-        logger.debug " * user_identifier: #{user_identifier}" if user_identifier.present?
-
-        user_token = config[:user_token]
-        logger.debug " * user_token: #{user_token}" if user_token.present?
->>>>>>> c8eb56c73da0329b3834b9f7f3c985dfbb503a0b
 
         connection = Faraday.new(:url => base_url,
                                  :request => {
@@ -61,17 +44,10 @@ module FinApps
           conn.request :multipart
           conn.request :url_encoded
           if user_identifier.blank? || user_token.blank?
-<<<<<<< HEAD
             logger.debug "##{__method__.to_s} => User credentials were not provided. Authentication header not set."
           else
             conn.request :basic_auth, user_identifier, user_token
             logger.debug "##{__method__.to_s} => Authentication header set for user: #{user_identifier}"
-=======
-            logger.debug ' * No user credentials provided. Authentication header not set.'
-          else
-            conn.request :basic_auth, user_identifier, user_token
-            logger.debug " * Authentication header set for user identifier: #{user_identifier}"
->>>>>>> c8eb56c73da0329b3834b9f7f3c985dfbb503a0b
           end
 
           # Response Middleware
@@ -93,11 +69,7 @@ module FinApps
         raise MissingArgumentsError.new 'Missing argument: host_url.' if host_url.blank?
         raise InvalidArgumentsError.new 'Invalid argument: host_url does not specify a valid protocol (http/https).' unless host_url.start_with?('http://', 'https://')
 
-<<<<<<< HEAD
         logger.debug "##{__method__.to_s} => host [#{host_url}] passed validation."
-=======
-        logger.debug " * #{host_url} is a valid host url for the API."
->>>>>>> c8eb56c73da0329b3834b9f7f3c985dfbb503a0b
       end
 
     end
