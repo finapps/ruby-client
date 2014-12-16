d3621334f9cc421f8f1e54f2c1b5d21cdb94a573
qbantek <qbantek@gmail.com>
2014-12-16 13:56:53 -0500

    transactions edit

--- lib/finapps/rest/transactions.rb
+++ lib/finapps/rest/transactions.rb
@@ -4,8 +4,37 @@ module FinApps
     class Transactions < FinApps::REST::Resources
       include FinApps::REST::Defaults
 
+      # @transaction_id [String]
+      # # @return [Hash, Array<String>]
+      def search_by_id(transaction_id)
+        logger.debug "##{__method__.to_s} => Started"
+
+        transactions, error_messages =  search_by_ids([transaction_id])
+
+        # get one
+        transaction = transactions.present? ? transactions.first : nil
+
+        logger.debug "##{__method__.to_s} => Completed"
+        return transaction, error_messages
+      end
+
+      # @transaction_ids [Array<String>]
+      # # @return [Array<Hash>, Array<String>]
+      def search_by_ids(transaction_ids)
+        logger.debug "##{__method__.to_s} => Started"
+
+        transactions, error_messages = search
+
+        # filter by given ids
+        transactions = transactions.present? ? transactions.select { |t| transaction_ids.include?(t[:_id]) } : nil
+
+        logger.debug "##{__method__.to_s} => Completed"
+        return transactions, error_messages
+      end
+
+
       # @param [Hash] params
-      # @return [Array<FinApps::REST::Transaction>, Array<String>]
+      # @return [Array<Hash>, Array<String>]
       def search(params={})
         logger.debug "##{__method__.to_s} => Started"
 
@@ -20,6 +49,7 @@ module FinApps
       end
 
       # @param [Hash] params
+      # @return [Array<Hash>, Array<String>]
       def edit(params={})
         logger.debug "##{__method__.to_s} => Started"
 
