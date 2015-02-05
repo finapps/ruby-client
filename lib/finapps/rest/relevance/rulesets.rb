85d104b8ebaf64fb34014422423c2191e17b7733
qbantek <qbantek@gmail.com>
2015-02-05 15:45:08 -0500

    bug fix

--- lib/finapps/rest/relevance/rulesets.rb
+++ lib/finapps/rest/relevance/rulesets.rb
@@ -51,7 +51,7 @@ module FinApps
           path = end_point
           logger.debug "##{__method__.to_s} => path: #{path}"
 
-          results, error_messages = @client.send(path, :post, params.compact)
+          results, error_messages = @client.send(path, :post, params)
 
           logger.debug "##{__method__.to_s} => Completed"
           return results, error_messages
--- lib/finapps/version.rb
+++ lib/finapps/version.rb
@@ -1,3 +1,3 @@
 module FinApps
-  VERSION = '0.14.1.pre'
+  VERSION = '0.14.5.pre'
 end
