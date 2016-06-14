376e89b975d63782ce6d3d6310ca9499de259805
Erich N. Quintero <erich@financialapps.com>
2016-06-14 13:54:55 -0400

    fixed budget calc

--- lib/finapps/rest/budget_calculation.rb
+++ lib/finapps/rest/budget_calculation.rb
@@ -6,13 +6,14 @@ module FinApps
     class BudgetCalculation < FinApps::REST::Resources
       include FinApps::REST::Defaults
 
+      # Creates a new Budget Calculation for the given user, using a budget template
+      # matching budget_model_id and teh given income amount.
       # @param [String] budget_model_id
       # @param [Number] income
       # @return [Array<Hash>, Array<String>]
       def create(budget_model_id, income)
         raise MissingArgumentsError.new 'Missing argument: budget_model_id.' if budget_model_id.blank?
         logger.debug "##{__method__.to_s} => budget_model: #{budget_model_id}"
-
         raise MissingArgumentsError.new 'Missing argument: income.' if income.blank?
         logger.debug "##{__method__.to_s} => income: #{income}"
 
@@ -22,16 +23,11 @@ module FinApps
         path = end_point.sub(':budget_model_id', ERB::Util.url_encode(budget_model_id)).sub(':income', ERB::Util.url_encode(income))
         logger.debug "##{__method__.to_s} => path: #{path}"
 
-<<<<<<< HEAD
-        budget_calculation, error_messages = @client.send(path, :get)
-=======
         budget_calculation, error_messages = @client.send_request(path, :get)
-        logger.debug "##{__method__.to_s} => Completed"
-
->>>>>>> develop
         return budget_calculation, error_messages
       end
 
+      # Shows the Budget Calculation for the given user.
       # @return [Array<Hash>, Array<String>]
       def show
         end_point = Defaults::END_POINTS[:budget_calculation_show]
@@ -40,13 +36,7 @@ module FinApps
         path = end_point
         logger.debug "##{__method__.to_s} => path: #{path}"
 
-<<<<<<< HEAD
-        budget_calculation, error_messages = @client.send(path, :get)
-=======
         budget_calculation, error_messages = @client.send_request(path, :get)
-        logger.debug "##{__method__.to_s} => Completed"
-
->>>>>>> develop
         return budget_calculation, error_messages
       end
 
