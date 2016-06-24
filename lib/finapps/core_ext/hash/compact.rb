f1a78553fa263a120ba971f1a065e12b33597f50
Erich Quintero <qbantek@gmail.com>
2016-06-24 18:46:30 -0400

    rf

--- lib/finapps/core_ext/hash/compact.rb
+++ lib/finapps/core_ext/hash/compact.rb
@@ -1,6 +1,6 @@
 # from rails/activesupport/lib/active_support/core_ext/hash/compact.rb
 
-class Hash
+class Hash # :nodoc:
   # Returns a hash with non +nil+ values.
   #
   #   hash = { a: true, b: false, c: nil}
@@ -8,7 +8,7 @@ class Hash
   #   hash # => { a: true, b: false, c: nil}
   #   { c: nil }.compact # => {}
   def compact
-    self.select { |_, value| !value.nil? }
+    select {|_, value| !value.nil? }
   end
 
   # Replaces current hash with non +nil+ values.
@@ -17,6 +17,6 @@ class Hash
   #   hash.compact! # => { a: true, b: false}
   #   hash # => { a: true, b: false}
   def compact!
-    self.reject! { |_, value| value.nil? }
+    reject! {|_, value| value.nil? }
   end
 end
