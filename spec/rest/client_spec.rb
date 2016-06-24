d4d93f33d8a1da754538af8d1b4e9616ffa8d8af
Erich Quintero <qbantek@gmail.com>
2016-06-24 18:46:45 -0400

    rf

--- spec/rest/client_spec.rb
+++ spec/rest/client_spec.rb
@@ -2,33 +2,27 @@ RSpec.describe FinApps::REST::Client do
   describe '#new' do
     it 'raises for missing company_identifier' do
       expect { FinApps::REST::Client.new nil, :company_token }
-          .to raise_error(FinApps::REST::MissingArgumentsError)
+        .to raise_error(FinApps::REST::MissingArgumentsError)
     end
 
     it 'raises for missing company_token' do
       expect { FinApps::REST::Client.new :company_identifier, nil }
-          .to raise_error(FinApps::REST::MissingArgumentsError)
+        .to raise_error(FinApps::REST::MissingArgumentsError)
     end
 
     it 'assigns @config' do
-
     end
 
-
     it 'assigns @logger' do
-
     end
   end
 
   describe '#connection' do
-
   end
 
   describe '#user_credentials!' do
-
   end
 
   describe '#send_request' do
-
   end
 end
