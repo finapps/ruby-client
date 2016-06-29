16f6d5b5df63474392621bc3fd9e7151701e6384
Erich N. Quintero <erich@financialapps.com>
2016-06-29 10:08:51 -0400

    errors

--- lib/finapps/error.rb
+++ lib/finapps/error.rb
@@ -1,5 +1,7 @@
-module FinApps
+module FinApps # :nodoc:
   class Error < StandardError; end
   class InvalidArgumentsError < Error; end
   class MissingArgumentsError < Error; end
+
+  %i(InvalidArgumentsError MissingArgumentsError).each {|const| Error.const_set(const, FinApps.const_get(const)) }
 end
