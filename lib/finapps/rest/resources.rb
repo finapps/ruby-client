dc1a5defe5ca6d3c1f3e15cfc5b7300a8de3d7b4
Erich N. Quintero <erich@financialapps.com>
2016-06-29 16:31:46 -0400

    users-wip

--- lib/finapps/rest/resources.rb
+++ lib/finapps/rest/resources.rb
@@ -8,6 +8,16 @@ module FinApps
       def initialize(client)
         @client = client
       end
+
+      def create(params, endpoint)
+        raise MissingArgumentsError.new 'Missing argument: params.' if params.blank?
+
+        path = Defaults::END_POINTS[endpoint]
+        logger.debug "#{name}##{__method__} => path: #{path} params: #{params}"
+
+        results, error_messages = client.send_request(path, :post, params)
+        [results, error_messages]
+      end
     end
   end
 end
--- lib/finapps/rest/users.rb
+++ lib/finapps/rest/users.rb
@@ -8,66 +8,35 @@ module FinApps
       # @return [FinApps::REST::User, Array<String>]
       def show(public_id)
         raise MissingArgumentsError.new 'Missing argument: public_id.' if public_id.blank?
-        logger.debug "##{__method__} => public_id: #{public_id}"
 
         end_point = Defaults::END_POINTS[:users_show]
-        logger.debug "##{__method__} => end_point: #{end_point}"
-
         path = end_point.sub ':public_id', ERB::Util.url_encode(public_id)
+
         logger.debug "##{__method__} => path: #{path}"
 
-        user, error_messages = client.send_request(path, :get) {|r| User.new(r.body) }
-        [user, error_messages]
+        results, error_messages = client.send_request(path, :get)
+        [results, error_messages]
       end
 
       # @param [Hash] params
       # @return [FinApps::REST::User, Array<String>]
-      def create(params={})
-        raise MissingArgumentsError.new 'Missing argument: params.' if params.blank?
-        logger.debug "##{__method__} => params: #{skip_sensitive_data params}"
-
-        end_point = Defaults::END_POINTS[:users_create]
-        logger.debug "##{__method__} => end_point: #{end_point}"
-
-        user, error_messages = client.send_request(end_point, :post, params) {|r| User.new(r.body) }
-        [user, error_messages]
+      def create(params)
+        super(params, :users_create)
       end
 
       # @param [Hash] params
       # @return [Array<String>]
-      def update(params={})
-        logger.debug "##{__method__} => params: #{skip_sensitive_data params}"
-
-        path = Defaults::END_POINTS[:users_update]
-        logger.debug "##{__method__} => path: #{path}"
-
-        _, error_messages = client.send_request(path, :put, params.compact)
-        error_messages
-      end
-
-      # @param [Hash] params
-      # @return [FinApps::REST::User, Array<String>]
-      def update_password(params={})
-        logger.debug "##{__method__} => params: #{skip_sensitive_data params}"
-
-        path = Defaults::END_POINTS[:users_update_password]
-        logger.debug "##{__method__} => path: #{path}"
-
-        user, error_messages = client.send_request(path, :put, params.compact) {|r| User.new(r.body) }
-        [user, error_messages]
-      end
-
-      # @param [Hash] params
-      # @return [FinApps::REST::User, Array<String>]
-      def login(params={})
+      def update(params)
         raise MissingArgumentsError.new 'Missing argument: params.' if params.blank?
-        logger.debug "##{__method__} => params: #{skip_sensitive_data params}"
+        logger.debug "##{__method__} => params: #{params}"
 
-        end_point = Defaults::END_POINTS[:users_login]
-        logger.debug "##{__method__} => end_point: #{end_point}"
+        end_point = Defaults::END_POINTS[:users_update]
+        path = end_point
 
-        user, error_messages = client.send_request(end_point, :post, params) {|r| User.new(r.body) }
-        [user, error_messages]
+        logger.debug "##{__method__} => path: #{path}"
+
+        results, error_messages = client.send_request(path, :put, params.compact)
+        [results, error_messages]
       end
 
       # @param [String] public_id
@@ -82,13 +51,9 @@ module FinApps
         path = end_point.sub ':public_id', ERB::Util.url_encode(public_id)
         logger.debug "##{__method__} => path: #{path}"
 
-        _, error_messages = client.send_request(path, :delete)
-        error_messages
+        results, error_messages = client.send_request(path, :delete)
+        [results, error_messages]
       end
-    end
-
-    class User < FinApps::REST::Resource # :nodoc:
-      attr_accessor :public_id, :token, :email, :first_name, :last_name, :postal_code
     end
   end
 end
