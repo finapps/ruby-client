de48c3a5e5ee2f276ce276f9283676564d640073
Erich N. Quintero <erich@financialapps.com>
2016-07-12 17:06:04 -0400

    tests

--- spec/rest/resources_spec.rb
+++ spec/rest/resources_spec.rb
@@ -1,9 +1,16 @@
 RSpec.describe FinApps::REST::Resources do
-
   describe '#new' do
-    subject { FinApps::REST::Resources.new(nil) }
-    it { expect { subject }.to raise_error(FinApps::MissingArgumentsError, 'Missing argument: client.') }
+    context 'when client is nil' do
+      subject { FinApps::REST::Resources.new(nil) }
+      it { expect { subject }.to raise_error(FinApps::MissingArgumentsError, 'Missing argument: client.') }
+    end
+
+    context 'when client param is set' do
+      let(:client) { FinApps::REST::Client.new :company_identifier, :company_token }
+      subject { FinApps::REST::Resources.new(client) }
+
+      it { expect { subject }.not_to raise_error }
+      it('assigns @client') { expect(subject.client).to eq(client) }
+    end
   end
-
-
 end
