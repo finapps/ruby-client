8c24d5602469fe9b47124e73ba56dc540d59533c
Erich Quintero <qbantek@gmail.com>
2016-07-15 16:22:44 -0400

    credentials

---
+++ lib/finapps/rest/credentials.rb
@@ -0,0 +1,20 @@
+module FinApps
+  module REST
+    # represents both tenant and user credentials
+    class Credentials
+      using ObjectExtensions
+      using StringExtensions
+
+      attr_reader :identifier, :token
+
+      def initialize(identifier, token)
+        @identifier = identifier
+        @token = token
+      end
+
+      def valid?
+        identifier.present? && token.present?
+      end
+    end
+  end
+end
