2c0490e06a22783c27b5d43b8f14091a5eebdeef
Erich Quintero <qbantek@gmail.com>
2016-07-15 17:41:21 -0400

    wip

--- lib/finapps/rest/connection.rb
+++ lib/finapps/rest/connection.rb
@@ -10,20 +10,17 @@ module FinApps
         }
 
         Faraday.new(options) do |conn|
-          # tenant level authentication
-          conn.use FinApps::Middleware::TenantAuthentication, config.tenant_credentials
-
-          # user level authentication
-          if config.valid_user_credentials?
-            conn.request :basic_auth, config.user_credentials[:identifier], config.user_credentials[:token]
-          end
-
+          conn.request :accept_json
+          conn.request :user_agent
+          conn.request :tenant_authentication, config.tenant_identifier, config.tenant_token
           conn.request :json
           conn.request :retry
           conn.request :multipart
           conn.request :url_encoded
-          conn.use FinApps::Middleware::AcceptJson
-          conn.use FinApps::Middleware::UserAgent
+          if FinApps::REST::Credentials.new(config.user_identifier, config.user_token).valid?
+            conn.request :basic_auth, config.user_identifier, config.user_token
+          end
+
           conn.use FinApps::Middleware::RaiseError
           conn.response :rashify
           conn.response :json, content_type: /\bjson$/
