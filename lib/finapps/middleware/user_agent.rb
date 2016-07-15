32a653a2de13de966b6787f565f739a15dbfa7cc
Erich Quintero <qbantek@gmail.com>
2016-07-15 17:01:11 -0400

    tests for user agent middleware

--- lib/finapps/middleware/user_agent.rb
+++ lib/finapps/middleware/user_agent.rb
@@ -1,13 +1,12 @@
 module FinApps
   module Middleware
     # This middleware sets the User-Agent request-header field to identify thei client.
-    # If the value for this header already exists, it is not overriden.
     class UserAgent < Faraday::Middleware
       KEY = 'User-Agent'.freeze unless defined? KEY
       RUBY = "#{RUBY_ENGINE}/#{RUBY_PLATFORM} #{RUBY_VERSION}-p#{RUBY_PATCHLEVEL}".freeze
 
       def call(env)
-        env[:request_headers][KEY] ||= "finapps-ruby/#{FinApps::VERSION} (#{RUBY})"
+        env[:request_headers][KEY] = "finapps-ruby/#{FinApps::VERSION} (#{RUBY})"
         @app.call(env)
       end
     end
