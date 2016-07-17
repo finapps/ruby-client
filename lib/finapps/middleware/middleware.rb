bf71110cda097e1f4b9d6d4de4eaa1d3610023e6
Erich N. Quintero <qbantek@gmail.com>
2016-07-17 15:08:25 -0400

    middleware file structure

--- lib/finapps/middleware/middleware.rb
+++ lib/finapps/middleware/middleware.rb
@@ -3,9 +3,9 @@ require 'faraday' unless defined? Faraday
 
 module FinApps
   module Middleware
-    autoload :AcceptJson, 'finapps/middleware/accept_json'
-    autoload :UserAgent, 'finapps/middleware/user_agent'
-    autoload :TenantAuthentication, 'finapps/middleware/tenant_authentication'
+    autoload :AcceptJson, 'finapps/middleware/request/accept_json'
+    autoload :UserAgent, 'finapps/middleware/request/user_agent'
+    autoload :TenantAuthentication, 'finapps/middleware/request/tenant_authentication'
 
     if Faraday::Middleware.respond_to? :register_middleware
       Faraday::Request.register_middleware \
