3a28d376895965dfd3d3a2e0a3fa787c422c0d16
Erich N. Quintero <qbantek@gmail.com>
2016-07-17 18:51:17 -0400

    better tests

--- spec/core_extensions/hash/compact_spec.rb
+++ spec/core_extensions/hash/compact_spec.rb
@@ -1,39 +1,21 @@
 # frozen_string_literal: true
-class RefinedHash < Hash
-  using HashExtensions
-  def compacted
-    compact
-  end
-
-  def compacted!
-    compact!
-  end
-end
-
 RSpec.describe HashExtensions do
-  let(:hash) do
-    RefinedHash.new
-  end
+  context 'when refining hash' do
+    using HashExtensions
 
-  before(:each) do
-    hash[:a] = true
-    hash[:b] = false
-    hash[:c] = nil
-  end
+    subject { {a: true, b: false, c: nil} }
 
-  describe '#compact' do
-    it 'returns a hash with non nil values' do
-      expect(hash.compacted).to eq(a: true, b: false)
-      expect(hash).to eq(a: true, b: false, c: nil)
+    describe '#compact' do
+      it 'returns a hash with non nil values' do
+        expect(subject.compact).to eq(a: true, b: false)
+        expect(subject).to eq(a: true, b: false, c: nil)
+      end
     end
-  end
-  describe '#compact!' do
-    it 'replaces current hash with non nil values' do
-      hash[:a] = true
-      hash[:b] = false
-      hash[:c] = nil
-      expect(hash.compacted!).to eq(a: true, b: false)
-      expect(hash).to eq(a: true, b: false)
+    describe '#compact!' do
+      it 'replaces current hash with non nil values' do
+        expect(subject.compact!).to eq(a: true, b: false)
+        expect(subject).to eq(a: true, b: false)
+      end
     end
   end
 end
