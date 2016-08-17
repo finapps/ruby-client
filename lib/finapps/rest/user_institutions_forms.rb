de9458a0c04f49a10542b8dfdf8f80e0e4edbd01
Anthony Mahadeo <anthony@financialapps.com>
2016-08-17 12:55:12 -0400

    Added User Institution Login (for credentials update) and User Institution Destroy

---
+++ lib/finapps/rest/user_institutions_forms.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+module FinApps
+  module REST
+    class UserInstitutionsForms < FinApps::REST::Resources
+      require 'erb'
+      using ObjectExtensions
+      using StringExtensions
+
+      def show(id)
+        raise MissingArgumentsError.new 'Missing argument: id.' if id.blank?
+
+        path = "institutions/user/#{ERB::Util.url_encode(id)}/form"
+        super id, path
+      end
+    end
+  end
+end
