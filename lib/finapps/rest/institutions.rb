51285b08555513300476899e20292b20be1d1827
Anthony Mahadeo <anthony@financialapps.com>
2016-08-25 15:07:35 -0400

    Moved (User)Institution Add Endpoint into User Institution Resource

--- lib/finapps/rest/institutions.rb
+++ lib/finapps/rest/institutions.rb
@@ -5,15 +5,6 @@ module FinApps
       using ObjectExtensions
       using StringExtensions
 
-      def create(site_id, params)
-        raise MissingArgumentsError.new 'Missing argument: site_id.' if site_id.blank?
-        raise MissingArgumentsError.new 'Missing argument: params.' if params.blank?
-
-        path = "#{end_point}/site/#{ERB::Util.url_encode(site_id)}/add"
-
-        super params, path
-      end
-
       def list(search_term)
         raise MissingArgumentsError.new 'Missing argument: search_term' if search_term.blank?
         # API errors when search_term is blank, maybe it shouldn't
