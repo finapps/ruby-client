b02ca3d2609ab3d803ab5be423e0753b8808d16b
Anthony Mahadeo <anthony@financialapps.com>
2017-03-23 10:42:36 -0400

    Add Operator Password Reset Endpoints

--- lib/finapps/rest/operators_password_resets.rb
+++ lib/finapps/rest/operators_password_resets.rb
@@ -3,10 +3,26 @@ module FinApps
     class OperatorsPasswordResets < FinAppsCore::REST::Resources
       def create(params, path=nil)
         not_blank(params, :params)
+        validates_email(params) if path.nil?
 
         path ||= "operators/password/forgot"
 
         super params, path
+      end
+
+      def update(params)
+        not_blank(params, :params)
+
+        path = "operators/password/reset"
+        create params, path
+      end
+
+      private
+
+      def validates_email(params)
+        unless params.key?(:email) && params[:email]
+          raise FinAppsCore::InvalidArgumentsError.new 'Invalid argument: params.'
+        end
       end
     end
   end
