a9d87b3b2df39a794a09c904dbdee4dbbb047c04
Anthony Mahadeo <anthony@financialapps.com>
2017-04-21 17:13:29 -0400

    rubocop

--- spec/utils/query_builder_spec.rb
+++ spec/utils/query_builder_spec.rb
@@ -11,7 +11,8 @@ RSpec.describe FinApps::Utils::QueryBuilder do
     context 'with full params' do
       let(:params) { {page: '1', requested: '20', sort: '-date', filter: '{"role": "1"}'} }
       it 'returns correct string' do
-        expect(subject.build_query_path(end_point, params)).to eq('orders?page=1&requested=20&sort=-date&filter=%7B%22role%22%3A%20%221%22%7D')
+        correct_string = 'orders?page=1&requested=20&sort=-date&filter=%7B%22role%22%3A%20%221%22%7D'
+        expect(subject.build_query_path(end_point, params)).to eq(correct_string)
       end
     end
 
