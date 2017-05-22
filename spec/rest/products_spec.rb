694456fde375d8c5cd00e7b7505197482b26da53
Anthony Mahadeo <anthony@financialapps.com>
2017-05-22 13:46:26 -0400

    create products resource spec

---
+++ spec/rest/products_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helpers/client'
+
+RSpec.describe FinApps::REST::Products, 'initialized with valid FinApps::Client object' do
+  include SpecHelpers::Client
+  subject(:products) { FinApps::REST::Operators.new(client) }
+
+  describe '#list' do
+    context 'when called' do
+      let(:list) { subject.list }
+      let(:results) { list[0] }
+      let(:error_messages) { list[1] }
+
+      it { expect { list }.not_to raise_error }
+      it('returns an array of records') { expect(results).to be_a(Array) }
+      it('returns no error messages') { expect(error_messages).to be_empty }
+    end
+  end
+end
+
