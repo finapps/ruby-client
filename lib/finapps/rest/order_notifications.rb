80ad29cae18d361c49920b19842cdef0e9739d30
Anthony Mahadeo <anthony@financialapps.com>
2017-05-25 11:44:39 -0400

    Create order notifications update instance method for resending notification

--- lib/finapps/rest/order_notifications.rb
+++ lib/finapps/rest/order_notifications.rb
@@ -1,7 +1,12 @@
 module FinApps
   module REST
-      class OrderNotifications < FinAppsCore::REST::Resources
+    class OrderNotifications < FinAppsCore::REST::Resources
+      def update(id)
+        not_blank(id, :id)
 
+        path = "orders/#{ERB::Util.url_encode(id)}/notify"
+        super nil, path
       end
+    end
   end
 end
