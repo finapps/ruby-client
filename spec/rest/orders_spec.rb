ea0f5d737fc1c8652ea1eee0b25f5200595cff52
Anthony Mahadeo <anthony@financialapps.com>
2017-05-25 15:26:38 -0400

    Add Order cancellation specs

--- spec/rest/orders_spec.rb
+++ spec/rest/orders_spec.rb
@@ -147,10 +147,19 @@ RSpec.describe FinApps::REST::Orders do
       let(:results) { destroy[RESULTS] }
       let(:error_messages) { destroy[ERROR_MESSAGES] }
 
-
       it { expect { destroy }.not_to raise_error }
       it('results is nil') { expect(destroy[0]).to be_nil }
       it('error messages array is populated') { expect(error_messages.first.downcase).to eq('resource not found') }
+    end
+
+    context 'for valid id' do
+      let(:destroy) { subject.destroy(:valid_id) }
+      let(:results) { destroy[RESULTS] }
+      let(:error_messages) { destroy[ERROR_MESSAGES] }
+
+      it { expect { destroy }.not_to raise_error }
+      it('results is nil') { expect(results).to be_nil }
+      it('error_messages array is empty') { expect(error_messages).to eq([]) }
     end
   end
 end
