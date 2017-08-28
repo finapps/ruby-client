da9657b45992206590ee8dce79f179db65e9ef17
Anthony Mahadeo <anthony@financialapps.com>
2017-08-28 15:13:45 -0400

    initialize order assignments spec

--- spec/rest/order_assignments_spec.rb
+++ spec/rest/order_assignments_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe FinApps::REST::OrderAssignments do
     subject(:order_assignments) { FinApps::REST::OrderAssignments.new(client) }
 
     context 'when missing id' do
-      let(:update) { subject.update(nil) }
+      let(:update) { subject.update(nil, []) }
       it('returns missing argument error') { expect { update }.to raise_error(FinAppsCore::MissingArgumentsError) }
     end
   end
