b3911e81ab806fdacc433a582390d8bb51232a8f
Anthony Mahadeo <anthony@financialapps.com>
2017-10-23 10:52:01 -0400

    add OrderRefresh#create method

--- lib/finapps/rest/order_refreshes.rb
+++ lib/finapps/rest/order_refreshes.rb
@@ -1,7 +1,10 @@
 module FinApps
   module REST
     class OrderRefreshes < FinAppsCore::REST::Resources
-
+      def create(id)
+        path = "orders/#{id}/refresh"
+        update nil, path
+      end
     end
   end
 end
