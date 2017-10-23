ab5f3a501ff81125f9dd517befaa9ac5917cd51e
Anthony Mahadeo <anthony@financialapps.com>
2017-10-23 11:25:15 -0400

    add order refresh fixture

--- spec/rest/order_refreshes_spec.rb
+++ spec/rest/order_refreshes_spec.rb
@@ -1,3 +1,24 @@
 require 'spec_helpers/client'
 
-RSpec.describe FinApps::REST::OrderRefreshes
+RSpec.describe FinApps::REST::OrderRefreshes do
+  include SpecHelpers::Client
+  subject { FinApps::REST::OrderRefreshes.new(client) }
+
+  describe '#create' do
+    context 'when missing id' do
+      let(:create) { subject.create(nil) }
+      it { expect { create }.to raise_error(FinAppsCore::MissingArgumentsError)}
+    end
+
+    context 'when valid id is provided' do
+      let(:create) { subject.create(:valid_id) }
+      it { expect { create }.not_to raise_error }
+      it('returns an array') { expect(create).to be_a(Array) }
+      it('performs a post and returns new refreshed order response') do
+        expect(create[RESULTS]).to respond_to(:public_id)
+        expect(create[RESULTS]).to respond_to(:original_order_id)
+      end
+      it('returns no error messages') { expect(create[ERROR_MESSAGES]).to be_empty }
+    end
+  end
+end
