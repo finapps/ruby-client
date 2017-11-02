cc6dcbec0f39678aa3a18abbab2c6fd6e1d379ad
Anthony Mahadeo <anthony@financialapps.com>
2017-11-02 14:05:31 -0400

    add statements resource w/ show method

---
+++ lib/finapps/rest/statements.rb
@@ -0,0 +1,13 @@
+module FinApps
+  module REST
+    class Statements < FinAppsCore::REST::Resources
+      def show(account_id, document_id)
+        not_blank(account_id, :account_id)
+        not_blank(document_id, :document_id)
+
+        path = "accounts/#{ERB::Util.url_encode(account_id)}/statement/#{ERB::Util.url_encode(document_id)}"
+        super nil, path
+      end
+    end
+  end
+end
