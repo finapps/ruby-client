d9b30bd15e2a93b6037fe2202dff0f86bd3b2918
Anthony Mahadeo <anthony@financialapps.com>
2018-01-10 12:01:31 -0500

    append to query builder spec

--- spec/utils/query_builder_spec.rb
+++ spec/utils/query_builder_spec.rb
@@ -2,11 +2,15 @@
 
 class FakeClass
   include FinApps::Utils::QueryBuilder
+
+  def build_filter(p)
+  end
 end
 
 RSpec.describe FinApps::Utils::QueryBuilder do
+  subject { FakeClass.new }
+
   describe '#build_query_path' do
-    subject { FakeClass.new }
     let(:end_point) { 'orders' }
 
     context 'with full params' do
@@ -22,6 +26,15 @@ RSpec.describe FinApps::Utils::QueryBuilder do
       it 'returns correct string' do
         expect(subject.build_query_path(end_point, params)).to eq('orders?page=1&filter=%7B%22role%22%3A%221%22%7D')
       end
+    end
+  end
+
+  describe '#set_filter' do
+    it 'sets filter key in params hash argument equal to #build_filter return' do
+      orig_params = {page: 2}
+      allow(subject).to receive(:build_filter) { 'foo' }
+      expect(subject).to receive(:build_filter).with(orig_params)
+      expect(subject.set_filter(orig_params)).to eq(orig_params.merge({filter: 'foo'}))
     end
   end
 end
