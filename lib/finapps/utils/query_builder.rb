cf814432557c1342cf64d65b466b27e0a7989c75
Anthony Mahadeo <anthony@financialapps.com>
2018-01-10 10:43:12 -0500

    repair querybuilder spec

--- lib/finapps/utils/query_builder.rb
+++ lib/finapps/utils/query_builder.rb
@@ -10,6 +10,9 @@ module FinApps
         filter = "filter=#{ERB::Util.url_encode(params[:filter].to_json)}" if params[:filter] && !params[:filter].empty?
         query_join(root_url, [page, requested, sort, filter])
       end
+
+      private
+
       def query_join(root_url, params_array)
         query_string = params_array.compact.join('&')
         !query_string.empty? ? [root_url, query_string].join('?') : nil
