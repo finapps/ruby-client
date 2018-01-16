ddaf39da1001edd17f72f43654022de2b03c5805
Erich Quintero <qbantek@gmail.com>
2018-01-16 10:35:48 -0500

    version bump to 3.x

--- spec/rest/orders_spec.rb
+++ spec/rest/orders_spec.rb
@@ -88,7 +88,7 @@ RSpec.describe FinApps::REST::Orders do
       it('returns no error messages') { expect(subject[ERROR_MESSAGES]).to be_empty }
       it 'builds query and sends proper request' do
         subject
-        url = "#{FinAppsCore::REST::Defaults::DEFAULTS[:host]}/v2/orders?filter=%7B%22$or%22:%5B%7B%22public_id%22" \
+        url = "#{FinAppsCore::REST::Defaults::DEFAULTS[:host]}/v3/orders?filter=%7B%22$or%22:%5B%7B%22public_id%22" \
               ':%22term%22%7D,%7B%22applicant.last_name%22:%22term%22%7D,%7B%22assignment.last_name%22:%22term%22%7D' \
               ',%7B%22requestor.reference_no%22:%22term%22%7D%5D,%22status%22:%7B%22$in%22:%5B1,7%5D%7D,%22' \
               'assignment.operator_id%22:%22valid_operator%22%7D&page=2&requested=25&sort=status'
@@ -97,7 +97,7 @@ RSpec.describe FinApps::REST::Orders do
       it 'builds query and sends proper request with searchTerm/relation exclusivity' do
         params[:searchTerm] = nil
         subject
-        url = "#{FinAppsCore::REST::Defaults::DEFAULTS[:host]}/v2/orders?filter=%7B%22status%22:%7B%22$in%22:%5B1," \
+        url = "#{FinAppsCore::REST::Defaults::DEFAULTS[:host]}/v3/orders?filter=%7B%22status%22:%7B%22$in%22:%5B1," \
               '7%5D%7D,%22assignment.operator_id%22:%22valid_operator%22,%22$or%22:%5B%7B%22public_id%22:%7B%22$in' \
               '%22:%5B%22valid_order_id%22%5D%7D%7D,%7B%22original_order_id%22:%7B%22$in%22:%5B%22valid_order_id%22' \
               '%5D%7D%7D%5D%7D&page=2&requested=25&sort=status'
@@ -106,7 +106,7 @@ RSpec.describe FinApps::REST::Orders do
       it 'builds null assignment query properly when supplied w/ empty string' do
         FinApps::REST::Orders.new(client).list(assignment: '')
 
-        url = "#{FinAppsCore::REST::Defaults::DEFAULTS[:host]}/v2/orders?filter=%7B%22assignment.operator_id%22:null%7D"
+        url = "#{FinAppsCore::REST::Defaults::DEFAULTS[:host]}/v3/orders?filter=%7B%22assignment.operator_id%22:null%7D"
         expect(WebMock).to have_requested(:get, url)
       end
     end
--- spec/support/fake_api.rb
+++ spec/support/fake_api.rb
@@ -5,32 +5,32 @@ require 'sinatra/base'
 # the FakeApi class is used to mock API requests while testing.
 class FakeApi < Sinatra::Base
   # resource
-  post('/v2/resources') { json_response 201, 'resource.json' }
-  get('/v2/resources/:id') { json_response 200, 'resource.json' }
-  get('/v2/resources') { json_response 200, 'resources.json' }
-  put('/v2/resources') { json_response 201, 'resource.json' }
-  delete('/v2/resources/:id') { status 202 }
+  post('/v3/resources') { json_response 201, 'resource.json' }
+  get('/v3/resources/:id') { json_response 200, 'resource.json' }
+  get('/v3/resources') { json_response 200, 'resources.json' }
+  put('/v3/resources') { json_response 201, 'resource.json' }
+  delete('/v3/resources/:id') { status 202 }
 
   # version
-  get('/v2/version') { 'Version => 2.1.29-.20161208.172810' }
+  get('/v3/version') { 'Version => 2.1.29-.20161208.172810' }
 
   # orders
-  post('/v2/orders/valid_token') { json_response 200, 'order_token.json' }
-  post('/v2/orders/invalid_token') { json_response 404, 'resource_not_found.json' }
-  get('/v2/orders/valid_id') { json_response 200, 'order.json' }
-  get('/v2/orders') { json_response 200, 'orders.json' }
-  get('/v2/orders/valid_id/report.:format') { json_response 200, 'order_report.json' }
-  get('/v2/orders/invalid_id/report.:format') { json_response 404, 'resource_not_found.json' }
-  get('/v2/orders/valid_id/status') { json_response 200, 'order_status.json' }
-  get('/v2/orders/invalid_id/status') { json_response 404, 'resource_not_found.json' }
-  put('/v2/orders/valid_id/cancel') { status 204 }
-  put('/v2/orders/invalid_id/cancel') { json_response 404, 'resource_not_found.json' }
-  put('/v2/orders/valid_id/notify') { status 204 }
-  put('/v2/orders/invalid_id/notify') { json_response 404, 'resource_not_found.json' }
-  put('/v2/orders/valid_id/refresh') { json_response 200, 'order_refresh.json' }
-  put('/v2/orders/invalid_id/refresh') { json_response 404, 'resource_not_found.json' }
-  put('/v2/orders/invalid_id') { json_response 404, 'resource_not_found.json' }
-  put('/v2/orders/valid_id') do
+  post('/v3/orders/valid_token') { json_response 200, 'order_token.json' }
+  post('/v3/orders/invalid_token') { json_response 404, 'resource_not_found.json' }
+  get('/v3/orders/valid_id') { json_response 200, 'order.json' }
+  get('/v3/orders') { json_response 200, 'orders.json' }
+  get('/v3/orders/valid_id/report.:format') { json_response 200, 'order_report.json' }
+  get('/v3/orders/invalid_id/report.:format') { json_response 404, 'resource_not_found.json' }
+  get('/v3/orders/valid_id/status') { json_response 200, 'order_status.json' }
+  get('/v3/orders/invalid_id/status') { json_response 404, 'resource_not_found.json' }
+  put('/v3/orders/valid_id/cancel') { status 204 }
+  put('/v3/orders/invalid_id/cancel') { json_response 404, 'resource_not_found.json' }
+  put('/v3/orders/valid_id/notify') { status 204 }
+  put('/v3/orders/invalid_id/notify') { json_response 404, 'resource_not_found.json' }
+  put('/v3/orders/valid_id/refresh') { json_response 200, 'order_refresh.json' }
+  put('/v3/orders/invalid_id/refresh') { json_response 404, 'resource_not_found.json' }
+  put('/v3/orders/invalid_id') { json_response 404, 'resource_not_found.json' }
+  put('/v3/orders/valid_id') do
     request.body.rewind
     request_payload = JSON.parse request.body.read
     if request_payload['accounts'] == 'valid_account'
@@ -39,7 +39,7 @@ class FakeApi < Sinatra::Base
       json_response 400, 'invalid_request_body.json'
     end
   end
-  post('/v2/orders') do
+  post('/v3/orders') do
     request.body.rewind
     request_payload = JSON.parse request.body.read
     if %w(applicant institutions product).all? {|s| request_payload.key? s }
@@ -50,57 +50,57 @@ class FakeApi < Sinatra::Base
   end
 
   # institutions
-  get('/v2/institutions/site/valid_site_id/form') { json_response 200, 'institution_login_form.json' }
-  get('/v2/institutions/site/invalid_site_id/form') { json_response 400, 'invalid_institution_id.json' }
-  post('/v2/institutions/site/valid_site_id/add') { json_response 200, 'institution_add.json' }
-  get('/v2/institutions/search/:search_term') { json_response 200, 'institutions_search_list.json' }
-  get('/v2/institutions/routing/:routing_number') { json_response 200, 'institutions_routing_number.json' }
-  get('/v2/institutions/site/:site_id') { json_response 200, 'institutions_routing_number.json' }
+  get('/v3/institutions/site/valid_site_id/form') { json_response 200, 'institution_login_form.json' }
+  get('/v3/institutions/site/invalid_site_id/form') { json_response 400, 'invalid_institution_id.json' }
+  post('/v3/institutions/site/valid_site_id/add') { json_response 200, 'institution_add.json' }
+  get('/v3/institutions/search/:search_term') { json_response 200, 'institutions_search_list.json' }
+  get('/v3/institutions/routing/:routing_number') { json_response 200, 'institutions_routing_number.json' }
+  get('/v3/institutions/site/:site_id') { json_response 200, 'institutions_routing_number.json' }
 
   # user institutions
-  get('/v2/institutions/consumer/valid_id/status') { json_response 200, 'user_institution_status.json' }
-  get('/v2/institutions/consumer/invalid_id/status') { json_response 400, 'invalid_user_institution_id.json' }
-  get('/v2/institutions/consumer') { json_response 200, 'user_institutions_list.json' }
-  get('/v2/institutions/consumer/valid_id') { json_response 200, 'user_institutions_show.json' }
-  get('/v2/institutions/consumer/invalid_id') { json_response 400, 'invalid_user_institution_id.json' }
-  put('/v2/institutions/consumer/refresh') { json_response 200, 'user_institutions_refresh_all.json' }
-  put('/v2/institutions/consumer/valid_id/credentials') { json_response 200, 'institution_add.json' }
-  put('/v2/institutions/consumer/invalid_id/credentials') { json_response 400, 'invalid_user_institution_id.json' }
-  put('/v2/institutions/consumer/valid_id/mfa') { json_response 200, 'institution_add.json' }
-  put('/v2/institutions/consumer/invalid_id/mfa') { json_response 400, 'invalid_user_institution_id.json' }
-  delete('/v2/institutions/consumer/valid_id') { status 204 }
-  delete('/v2/institutions/consumer/invalid_id') { json_response 400, 'invalid_user_institution_id.json' }
-  get('/v2/institutions/consumer/valid_id/form') { json_response 200, 'institution_login_form.json' }
-  get('/v2/institutions/consumer/invalid_id/form') { json_response 400, 'invalid_institution_id.json' }
-  put('/v2/institutions/refresh') { json_response 200, 'user_institution_refresh.json' }
+  get('/v3/institutions/consumer/valid_id/status') { json_response 200, 'user_institution_status.json' }
+  get('/v3/institutions/consumer/invalid_id/status') { json_response 400, 'invalid_user_institution_id.json' }
+  get('/v3/institutions/consumer') { json_response 200, 'user_institutions_list.json' }
+  get('/v3/institutions/consumer/valid_id') { json_response 200, 'user_institutions_show.json' }
+  get('/v3/institutions/consumer/invalid_id') { json_response 400, 'invalid_user_institution_id.json' }
+  put('/v3/institutions/consumer/refresh') { json_response 200, 'user_institutions_refresh_all.json' }
+  put('/v3/institutions/consumer/valid_id/credentials') { json_response 200, 'institution_add.json' }
+  put('/v3/institutions/consumer/invalid_id/credentials') { json_response 400, 'invalid_user_institution_id.json' }
+  put('/v3/institutions/consumer/valid_id/mfa') { json_response 200, 'institution_add.json' }
+  put('/v3/institutions/consumer/invalid_id/mfa') { json_response 400, 'invalid_user_institution_id.json' }
+  delete('/v3/institutions/consumer/valid_id') { status 204 }
+  delete('/v3/institutions/consumer/invalid_id') { json_response 400, 'invalid_user_institution_id.json' }
+  get('/v3/institutions/consumer/valid_id/form') { json_response 200, 'institution_login_form.json' }
+  get('/v3/institutions/consumer/invalid_id/form') { json_response 400, 'invalid_institution_id.json' }
+  put('/v3/institutions/refresh') { json_response 200, 'user_institution_refresh.json' }
 
   # consumers
-  get('/v2/consumers/valid_public_id') { json_response 200, 'user.json' }
-  get('/v2/consumers/invalid_public_id') { json_response 404, 'resource_not_found.json' }
-  put('/v2/consumers/valid_public_id') { status 204 }
-  put('/v2/consumers/invalid_public_id') { json_response 400, 'invalid_user_id.json' }
-  put('/v2/consumers/valid_public_id/password') { json_response 200, 'user.json' }
-  put('/v2/consumers/invalid_public_id/password') { json_response 404, 'resource_not_found.json' }
-  delete('/v2/consumers/valid_public_id') { status 204 }
-  delete('/v2/consumers/invalid_public_id') { json_response 404, 'resource_not_found.json' }
+  get('/v3/consumers/valid_public_id') { json_response 200, 'user.json' }
+  get('/v3/consumers/invalid_public_id') { json_response 404, 'resource_not_found.json' }
+  put('/v3/consumers/valid_public_id') { status 204 }
+  put('/v3/consumers/invalid_public_id') { json_response 400, 'invalid_user_id.json' }
+  put('/v3/consumers/valid_public_id/password') { json_response 200, 'user.json' }
+  put('/v3/consumers/invalid_public_id/password') { json_response 404, 'resource_not_found.json' }
+  delete('/v3/consumers/valid_public_id') { status 204 }
+  delete('/v3/consumers/invalid_public_id') { json_response 404, 'resource_not_found.json' }
 
   # accounts
-  get('/v2/accounts/valid_id/statement/valid_id') { json_response 200, 'fake_pdf_statement.json' }
-  get('/v2/accounts/invalid_id/statement/valid_id') { json_response 404, 'resource_not_found.json' }
+  get('/v3/accounts/valid_id/statement/valid_id') { json_response 200, 'fake_pdf_statement.json' }
+  get('/v3/accounts/invalid_id/statement/valid_id') { json_response 404, 'resource_not_found.json' }
 
   # operators
-  get('/v2/operators') { json_response 200, 'operator_list.json' }
-  get('/v2/operators/invalid_id') { json_response 404, 'resource_not_found.json' }
-  get('/v2/operators/valid_id') { json_response 200, 'operator.json' }
-  delete('/v2/operators/invalid_id') { json_response 404, 'resource_not_found.json' }
-  delete('/v2/operators/valid_id') { status 204 }
-  post('/v2/operators/password/change') { json_response 200, 'operator.json' }
-  put('/v2/operators/invalid_id') { json_response 404, 'resource_not_found.json' }
-  put('/v2/operators/valid_id') { json_response 200, 'operator.json' }
-  put('/v2/operators/valid_id/assign') { status 204 }
-  put('/v2/operators/invalid_id/assign') { json_response 404, 'resource_not_found.json' }
-  post('/v2/operators/password/forgot') { json_response 200, 'operator_forgot_password.json' }
-  post('/v2/operators/password/reset')  do
+  get('/v3/operators') { json_response 200, 'operator_list.json' }
+  get('/v3/operators/invalid_id') { json_response 404, 'resource_not_found.json' }
+  get('/v3/operators/valid_id') { json_response 200, 'operator.json' }
+  delete('/v3/operators/invalid_id') { json_response 404, 'resource_not_found.json' }
+  delete('/v3/operators/valid_id') { status 204 }
+  post('/v3/operators/password/change') { json_response 200, 'operator.json' }
+  put('/v3/operators/invalid_id') { json_response 404, 'resource_not_found.json' }
+  put('/v3/operators/valid_id') { json_response 200, 'operator.json' }
+  put('/v3/operators/valid_id/assign') { status 204 }
+  put('/v3/operators/invalid_id/assign') { json_response 404, 'resource_not_found.json' }
+  post('/v3/operators/password/forgot') { json_response 200, 'operator_forgot_password.json' }
+  post('/v3/operators/password/reset')  do
     request.body.rewind
     request_payload = JSON.parse request.body.read
     if request_payload['params'] == 'valid'
@@ -109,7 +109,7 @@ class FakeApi < Sinatra::Base
       json_response 400, 'invalid_request_body.json'
     end
   end
-  post('/v2/operators') do
+  post('/v3/operators') do
     request.body.rewind
     request_payload = JSON.parse request.body.read
     if request_payload['params'] == 'valid'
@@ -120,7 +120,7 @@ class FakeApi < Sinatra::Base
   end
 
   # session
-  post('/v2/login') do
+  post('/v3/login') do
     request.body.rewind
     request_payload = JSON.parse request.body.read
     if request_payload['password'] == 'valid_password'
@@ -129,12 +129,12 @@ class FakeApi < Sinatra::Base
       json_response(401, 'unauthorized.json')
     end
   end
-  post('/v2/operators/login') { json_response 200, 'operator.json' }
+  post('/v3/operators/login') { json_response 200, 'operator.json' }
 
   # password resets
-  post('/v2/tenant/valid_user_id/password') { json_response 200, 'password_reset_token.json' }
-  post('/v2/tenant/invalid_user_id/password') { json_response 404, 'resource_not_found.json' }
-  put('/v2/tenant/valid_user_id/password') do
+  post('/v3/tenant/valid_user_id/password') { json_response 200, 'password_reset_token.json' }
+  post('/v3/tenant/invalid_user_id/password') { json_response 404, 'resource_not_found.json' }
+  put('/v3/tenant/valid_user_id/password') do
     request.body.rewind
     request_payload = JSON.parse request.body.read
     if request_payload['token'] == 'valid_token'
@@ -143,21 +143,21 @@ class FakeApi < Sinatra::Base
       json_response(400, 'invalid_request_body.json')
     end
   end
-  put('/v2/tenant/invalid_user_id/password') { json_response 404, 'resource_not_found.json' }
+  put('/v3/tenant/invalid_user_id/password') { json_response 404, 'resource_not_found.json' }
 
   # products
-  get('/v2/products') { json_response 200, 'products.json' }
+  get('/v3/products') { json_response 200, 'products.json' }
 
   # relevance
-  get('/v2/relevance/ruleset/names') { json_response 200, 'relevance_ruleset_names.json' }
+  get('/v3/relevance/ruleset/names') { json_response 200, 'relevance_ruleset_names.json' }
 
   # errors
-  get('/v2/client_error') { json_response 400, 'error.json' }
-  get('/v2/server_error') { status 500 }
-  get('/v2/proxy_error') { status 407 }
+  get('/v3/client_error') { json_response 400, 'error.json' }
+  get('/v3/server_error') { status 500 }
+  get('/v3/proxy_error') { status 407 }
 
   # timeout
-  get('/v2/orders/timeout') { status 419 }
+  get('/v3/orders/timeout') { status 419 }
 
   private
 
