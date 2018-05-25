c1401130b3ff3ab81ae87f64d72290d92d2dc65f
Anthony Mahadeo <anthony@financialapps.com>
2018-05-25 12:06:55 -0400

    add Session#destroy

--- lib/finapps/rest/sessions.rb
+++ lib/finapps/rest/sessions.rb
@@ -3,13 +3,19 @@
 module FinApps
   module REST
     class Sessions < FinAppsCore::REST::Resources # :nodoc:
+      LOGOUT = 'logout'
       # @param [Hash] params
       # @return [Array<String>]
       def create(params, path=nil)
+        return super nil, path if path == LOGOUT
         raise FinAppsCore::InvalidArgumentsError.new 'Invalid argument: params.' unless validates params
         path ||= 'login'
 
         super params, path
+      end
+
+      def destroy
+        create nil, LOGOUT
       end
 
       private
