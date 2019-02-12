818f6d3cf8a1f7ca03c27fd108ca4818736ee61d
Anthony Mahadeo <anthony@financialapps.com>
2019-02-12 12:01:59 -0500

    add crud methods for portfolios

---
+++ lib/finapps/rest/portfolios.rb
@@ -0,0 +1,41 @@
+module FinApps
+  module REST
+    class Portfolios < FinAppsCore::REST::Resources
+      include FinApps::Utils::QueryBuilder
+
+      def list(params = nil)
+        return super if params.nil?
+        raise FinAppsCore::InvalidArgumentsError, 'Invalid argument: params' unless params.is_a? Hash
+
+        super build_query_path(end_point, params)
+      end
+
+      def show(id)
+        not_blank(id, :id)
+        super
+      end
+
+      def create(params)
+        not_blank(params, :params)
+        super params
+      end
+
+      def update(id, params)
+        not_blank(id, :id)
+        not_blank(params, :params)
+
+        path = "#{end_point}/#{ERB::Util.url_encode(id)}"
+
+        super params, path
+      end
+
+      def destroy(id)
+        not_blank(id, :id)
+
+        super
+      end
+
+      # need to create build_filter method
+    end
+  end
+end
